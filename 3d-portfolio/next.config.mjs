5a9c759f9ef3f4a4b1cd8482f6fb7f31766b7d8e
Nazarii Dovhan <nazardov1@gmail.com>
2026-02-17 14:14:53 +0100

    Fix Pages deploy config

--- 3d-portfolio/next.config.mjs
+++ 3d-portfolio/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true
-  }
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
 };
 
 export default nextConfig;

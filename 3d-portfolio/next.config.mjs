4b22dcab1f9b2361a145cbb6ac1da157ed39f337
Nazarii Dovhan <nazardov1@gmail.com>
2026-02-17 14:52:35 +0100

    Fix _next paths for GitHub Pages

--- 3d-portfolio/next.config.mjs
+++ 3d-portfolio/next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+const repo = "3D-Portfolio";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
+
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
 
   eslint: {
     ignoreDuringBuilds: true,
